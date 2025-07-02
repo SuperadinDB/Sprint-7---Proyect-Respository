9f4f663d905a9bf98299b6e88d43fd43682ac46d
SuperadinDB <191742298+SuperadinDB@users.noreply.github.com>
2025-07-02 10:24:22 -0600

    Lanzamiento de aplicación para EDA de vehicules_us - V1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Sprint-7---Proyect-Respository
+¡Hola a todos! Soy Nahuel Soliz y me alegra que se hayan tomado un tiempo para revisar este README.
+
+En este repositorio se llevará a cabo la puesta en escena del poryecto del sprint 7 que desarrolle para el Bootcamp de Tripleten - DS 44 / 2025
+
+Dicho proyecto consiste en la implementación de cero, de una aplicación web gestionada por medio de un repositorio de GitHub y ejecutada en Render.
+
+La misma ejecuta una análisis exploratorio de datos (EDA) para la visualización de dátos del dataframe original 'vehicles_us.csv', que contiene información histórica de la cantidad de anuncios publicitarios de vehiculos lanzados al mercado por distintas automotrices, a lo largo de la historia.
+
+Espero les guste y ¡nos vemos pronto!
+
+Saludos. 
---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us_cleaned.csv')
+
+st.header("Análisis exploratorio de datos")
+st.write("Esta aplicación, realiza un análisis exploratorio de datos del dataset \
+         'vehicles_us_cleaned.csv', que es el dataframe 'vehicles_us', \
+         ya procesado y con el formato adecuado para su visualización.")
+
+show_hist_button = st.checkbox("Construir histograma")
+
+if show_hist_button:
+    st.write("Este histograma muestra la distribución de los precios de los vehículos en el dataset.")
+    fig = px.histogram(df, x="price", nbins=50, title="Distribución de precios de vehículos")
+    st.plotly_chart(fig)
+
+show_scatter_button = st.checkbox("Construir gráfico de dispersión")
+
+if show_scatter_button:
+    st.write("Este gráfico de dispersión muestra la relación entre el año del modelo y el precio de los vehículos.")
+    fig = px.scatter(df, x="model_year", y="price", title="Relación entre año del modelo y precio")
+    st.plotly_chart(fig)
+
+
+
